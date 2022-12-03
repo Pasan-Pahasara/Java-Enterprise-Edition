3fb0c2925f9bcb16f7978469dc939a6ca6727522
unknown <pasanpahasara7788@gmail.com>
2022-12-04 01:27:49 +0530

    Introducing All Mapping Specification In Java EE

---
+++ 05_Mapping-Spec/src/servelet/CustomerServlet.java
@@ -0,0 +1,22 @@
+package servelet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Pasan Pahasara
+ * @time : ${TIME}
+ * @date : ${DATE}
+ * @since : 0.1.0
+ **/
+@WebServlet(urlPatterns = "/customer") //Exact Mapping
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Exact Mapping Invoked");
+    }
+}
---
+++ 05_Mapping-Spec/src/servelet/EmptyStringMapping.java
@@ -0,0 +1,22 @@
+package servelet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Pasan Pahasara
+ * @time : 1:53 PM
+ * @date : 11/25/2022
+ * @since : 0.1.0
+ **/
+@WebServlet(urlPatterns = "") //Empty string Mapping
+public class EmptyStringMapping extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Empty string Mapping Invoked");
+    }
+}
