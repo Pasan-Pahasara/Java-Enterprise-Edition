1d655b802cc48604540a92e6ad4703453831c32b
unknown <pasanpahasara7788@gmail.com>
2022-12-04 01:48:31 +0530

    sendRedirect customer using Empty String Mapping In Java EE

--- 05_Mapping-Spec/src/servelet/EmptyStringMapping.java
+++ 05_Mapping-Spec/src/servelet/EmptyStringMapping.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 public class EmptyStringMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Empty string Mapping Invoked");
+//        resp.getWriter().write("Empty string Mapping Invoked");
+        resp.sendRedirect("customer");
     }
 }
