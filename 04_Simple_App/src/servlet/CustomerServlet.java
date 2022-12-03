cd38b7aa82f5fa8ee9dab35be748cf17a12f0d1f
unknown <pasanpahasara7788@gmail.com>
2022-12-03 23:14:20 +0530

    Refactor get details database using PostMethod in the Simple_App 04

--- 04_Simple_App/src/servlet/CustomerServlet.java
+++ 04_Simple_App/src/servlet/CustomerServlet.java
@@ -31,15 +31,41 @@ public class CustomerServlet extends HttpServlet {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/JEPOS", "root", "1234");
             PreparedStatement pstm = connection.prepareStatement("insert into Customer values(?,?,?,?)");
-            pstm.setObject(1,id);
-            pstm.setObject(2,name);
-            pstm.setObject(3,address);
-            pstm.setObject(4,salary);
+            pstm.setObject(1, id);
+            pstm.setObject(2, name);
+            pstm.setObject(3, address);
+            pstm.setObject(4, salary);
             boolean b = pstm.executeUpdate() > 0;
 
             PrintWriter writer = resp.getWriter();
-            writer.write("<h1>Customer Added State : "+b+"</h1>");
+            writer.write("<h1>Customer Added State : " + b + "</h1>");
 
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        double salary = Double.parseDouble(req.getParameter("salary"));
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/JEPOS", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("insert into Customer values(?,?,?,?)");
+            pstm.setObject(1, id);
+            pstm.setObject(2, name);
+            pstm.setObject(3, address);
+            pstm.setObject(4, salary);
+            boolean b = pstm.executeUpdate() > 0;
+
+            PrintWriter writer = resp.getWriter();
+            writer.write("<h1>Customer Added State : " + b + "</h1>");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (SQLException e) {
