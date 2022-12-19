ff9e16ad50e61d5552b65e1ebb8b53da5d07ac30
unknown <pasanpahasara7788@gmail.com>
2022-12-19 12:01:41 +0530

    Content Updated

--- 09_Filters/src/filters/FilterFour.java
+++ 09_Filters/src/filters/FilterFour.java
@@ -6,25 +6,26 @@ import java.io.IOException;
 
 /**
  * @author : ShEnUx
- * @time : 10:11 AM
+ * @time : 11:41 AM
  * @date : 12/19/2022
  * @since : 0.1.0
  **/
-//@WebFilter(urlPatterns = "/a")
-public class FilterOne implements Filter {
+@WebFilter(urlPatterns = "/a")
+public class FilterFour implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("Filter One Initialized");
+
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("Filter One Do Filter Method Invoked");
-        servletResponse.getWriter().print("<h1>Request Intercept By Filter One</h1>");
+        System.out.println("Do Filter Invoked 1");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("Do Filter Invoked 2");
     }
 
     @Override
     public void destroy() {
-        System.out.println("Filter One Destroyed");
+
     }
 }
--- 09_Filters/src/filters/FilterThree.java
+++ 09_Filters/src/filters/FilterThree.java
@@ -6,30 +6,33 @@ import java.io.IOException;
 
 /**
  * @author : ShEnUx
- * @time : 10:22 AM
+ * @time : 11:23 AM
  * @date : 12/19/2022
  * @since : 0.1.0
  **/
-//@WebFilter(urlPatterns = "/b")
-public class FilterTwo implements Filter {
+@WebFilter(urlPatterns = "/b")
+public class FilterThree implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("Filter Two Initialized");
+        System.out.println("Filter Three Initialized");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("Filter Two Do Filter Method Invoked");
+        System.out.println("Filter Three Do Filter Method Invoked");
         String name = servletRequest.getParameter("name");
         if (name.equals("ijse")) {
+            //dispatch request to the servlet
+            filterChain.doFilter(servletRequest,servletResponse);
+            //append the response
             servletResponse.getWriter().write("<h1>Authenticated User</h1>");
-        } else {
+        }else {
             servletResponse.getWriter().write("<h1>Non Authenticated User</h1>");
         }
     }
 
     @Override
     public void destroy() {
-        System.out.println("Filter Two Destroyed");
+        System.out.println("Filter Three Destroyed");
     }
 }
