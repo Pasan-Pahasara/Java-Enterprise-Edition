7e94b4c71bd21306fb36381a28ac3a887995d5e9
unknown <pasanpahasara7788@gmail.com>
2022-12-23 10:46:53 +0530

    Added Listeners Module and introduce MyListener class

---
+++ 13_Listeners/src/listeners/MyListener.java
@@ -0,0 +1,24 @@
+package listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * @author : ShEnUx
+ * @time : 10:35 AM
+ * @date : 12/23/2022
+ * @since : 0.1.0
+ **/
+@WebListener
+public class MyListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("Servlet Context Created");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("Servlet Context Destroyed");
+    }
+}
--- 13_Listeners/src/servlet/ServletA.java
+++ 13_Listeners/src/servlet/ServletA.java
@@ -1,3 +1,5 @@
+package servlet;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
